49cf97411222d8383efba453a2d9d8af2d2e0dcb
blueskyzii <yasutecoy@gmail.com>
2026-01-09 10:44:29 +0700

    Fix path file

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ console.log("Aplikasi LKS Cloud Computing berhasil dimuat");
 
 function home() {
     console.log("Anda berada di halaman Home");
-    document.location.href = "index.html";
+    document.location.href = "../index.html";
 }
 
 function about() {
     console.log("Anda berada di halaman About");
-    document.location.href = "about.html";
+    document.location.href = "../about.html";
 }
